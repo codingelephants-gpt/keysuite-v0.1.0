729eb3c7341eaf89bff697b77225bf66230d33c3
codingelephants <florismooij2014@gmail.com>
2024-12-17 18:44:22 +0100

    Update core.py

--- keysuite/core.py
+++ keysuite/core.py
@@ -7,9 +7,9 @@ def newpass(length=12):
     return ''.join(random.choice(characters) for _ in range(length))
 
 def newpass_nl(length=12):
-    key_num = zfill(random.randint(0, 999999), 6)
-    key_string = ''.join(random.choices(string.ascii_letters, k=6))
-    key = key_num + key_string
-    key_list = list(key)
-    key = random.shuffle(key_list)
-    
+    key_num = str(random.randint(0, 999999)).zfill(6)
+	key_string = ''.join(random.choices(string.ascii_letters, k=6))
+	key = key_num + key_string
+	key_list = list(key)
+	random.shuffle(key_list)
+	return ''.join(key_list)
