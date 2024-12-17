87e9b1a7ee97016bf1d42a75e74818d50fe633a3
codingelephants <florismooij2014@gmail.com>
2024-12-17 17:17:01 +0100

    Update core.py

--- keysuite/core.py
+++ keysuite/core.py
@@ -1,29 +1,8 @@
-import random as rng
-import string
-def newpass():
-	num = rng.randint(0, 99999999)
-	
-	num = str(num).zfill(8)
-		
-	length = 8
-	
-	letters = string.ascii_letters + string.digits
-	full_ltr = ''.join(rng.choices(letters, k=length))
-	
-	
-	password = num + full_ltr
-	
-	password_list = list(password)
-	rng.shuffle(password_list)
-	shuffled_password = ''.join(password_list)
-	
-	return shuffled_password
-	
-print(newpass())
-	
+# core.py
 
-"""
-Password Generator in python
-
-PROPERTY OF codingelephants-gpt ON https://github.com/codingelephants-gpt/pgen1.0.0
-"""
+def newpass(length=12):
+    """Generate a random password."""
+    import random
+    import string
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
