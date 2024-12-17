8e9bdf894d458689fb0f4adfd810396cff6c21a9
codingelephants <florismooij2014@gmail.com>
2024-12-17 18:38:56 +0100

    Update core.py

--- keysuite/core.py
+++ keysuite/core.py
@@ -1,8 +1,15 @@
 # core.py
-
+import random
+import string
 def newpass(length=12):
     """Generate a random password."""
-    import random
-    import string
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for _ in range(length))
+
+def newpass_nl(length=12):
+    key_num = zfill(random.randint(0, 999999), 6)
+    key_string = ''.join(random.choices(string.ascii_letters, k=6))
+    key = key_num + key_string
+    key_list = list(key)
+    key = random.shuffle(key_list)
+    
