45afe7b23ba4aafae2158dcd3753da96c4e023b3
codingelephants <florismooij2014@gmail.com>
2024-12-18 07:14:08 +0100

    Create intr.py

---
+++ keysuite/intr.py
@@ -0,0 +1,9 @@
+class keyintro:
+	def __init__(self):
+		self.help = """Welcome to KeySuite v0.2.0!
+newpass() - Generates a new passkey with letters, numbers and symbols.
+newpass_nl() - Generates a new passkey with letter and numbers.
+varsh(var) - Shuffles a string.
+		"""
+		
+intro = keyintro()
