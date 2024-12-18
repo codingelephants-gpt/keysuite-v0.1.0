c7869fe395163349c9736c0e379e9865eb36a039
codingelephants <florismooij2014@gmail.com>
2024-12-18 07:36:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="keysuite",
     version="0.2.0",
+    author='codingelephants-gpt',
+    url='https://github.com/codingelephants-gpt/keysuite-v0.2.0/'
+    description='Key Generator Module for python',
+    long_description=open('README.md').read()
     packages=find_packages(),  # Automatically find all packages
 )
