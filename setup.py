c8cc1ed42d1710afec66344c39577fdf0c51cd99
codingelephants <florismooij2014@gmail.com>
2024-12-18 07:15:46 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="keysuite",
-    version="0.1.5",
+    version="0.2.0",
     packages=find_packages(),  # Automatically find all packages
 )
