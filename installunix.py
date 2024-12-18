a1bf4346cf209510cf0abc12283816d8c61b2f5f
codingelephants <florismooij2014@gmail.com>
2024-12-18 13:19:40 +0100

    Create installunix.py

---
+++ installunix.py
@@ -0,0 +1,8 @@
+try:
+  import os
+except exception as e:
+	print(f'Error: {e}')
+
+print("Enter your password here for security reasons:")
+os.system('sudo apt install "pip"')
+os.system('pip install "."')
